d69d75b56febdb3a510ddf5474d9bb37c7772271
Krasi <krasi2405@gmail.com>
2016-06-20 00:39:13 +0300

    Complete version of website

--- sticky_navbar.js
+++ sticky_navbar.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     var origOffsetY = $('.menu').offset().top;
-
+  
     function scroll() {
         if ($(window).scrollTop() >= origOffsetY) {
             $('.menu').addClass('navbar-fixed-top');
-            $('.content').css('padding-top', $('nav').height);
+            var padding_top_value = $('.menu').height() + "px";
+            console.log(padding_top_value);
+            $('.content').css('padding-top', padding_top_value);
         } else {
             $('.menu').removeClass('navbar-fixed-top');
             $('.content').css('padding-top', 0);
